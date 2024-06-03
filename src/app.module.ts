b5d11ddeab30ca1dba61d9cc15e0ed3e92a26d81
iangalmiche4 <galmicheian@gmail.com>
2024-06-03 10:47:31 +0200

    Add BullMQ

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { BullQueueModule } from './bull/bull.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthCheckModule } from './healthCheck/healthCheck.module';
 
 @Module({
-  imports: [HealthCheckModule],
+  imports: [BullQueueModule, HealthCheckModule],
   controllers: [AppController],
   providers: [AppService],
 })
